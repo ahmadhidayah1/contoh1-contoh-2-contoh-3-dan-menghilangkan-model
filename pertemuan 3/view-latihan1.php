0b75d8f4bf2980dee74132e740882810e5bba71e
ahmadhidayah1 <90608522+ahmadhidayah1@users.noreply.github.com>
2021-11-21 16:47:58 +0700

    Add files via upload

---
+++ pertemuan 3/view-latihan1.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>latihan 1</title>
+
+</head>
+
+<body>
+    Halo kawan.. yuk kita pusing bersama..!!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+    $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+    </body>
+
+    </html>
